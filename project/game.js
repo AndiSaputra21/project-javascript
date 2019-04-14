a09776e1838f8b361dbbe3aaf9ce375b2363d2c8
Andi Syaputra <46291588+AndiSaputra21@users.noreply.github.com>
2019-04-14 13:40:40 +0700

    tugas javascript

---
+++ project/game.js
@@ -0,0 +1,144 @@
+//.var nama = "Andi"
+//var peran = " "
+
+// var nama = ''
+// var peran =''
+
+// if (nama === '') {
+// 	console.log('Masukan Nama Di sini')
+// }
+// else {
+// 	console.log('Selamat')
+// }
+
+// var nama ="Andi"
+// var peran = ""
+
+// if (nama === "Andi") {
+// 	console.log("Halo Andi, Pilih Peranmu untuk memulai game!")
+// }
+// else{
+// }
+
+// var nama ="Andi"
+// var peran="Ksatria"
+
+// if (nama !== peran) {
+// 	console.log("Selamat datang di Dunia Proxytia, Andi")
+// }
+// else{
+// 	console.log("Halo Ksatria Andi, kamu dapat menyerang denngan senjatamu!")
+// }
+// if (peran === "Ksatria") {
+// 	console.log("Halo Ksatria Andi, kamu dapat menyerang dengan senjatamu")
+// }
+// else{
+// 	console.log("Hajar terus")
+// }
+
+// var nama ="Ellen"
+// var peran="Tabib"
+
+// if (nama !== peran) {
+// 	console.log("Selamat datang di Dunia Proxytia, Ellen")
+// }
+// else{
+// 	console.log("Halo Ksatria Andi, kamu dapat menyerang denngan senjatamu!")
+// }
+// if (peran === "Tabib") {
+// 	console.log("Halo Ksatria Ellen, kamu dapat membantu temanmu yang terluka")
+// }
+// else{
+// 	console.log("Hajar terus")
+// }
+
+// var nama ="Ellen"
+// var peran="Tabib"
+
+// if (nama !== peran) {
+// 	console.log("Selamat datang di Dunia Proxytia, Ellen")
+// }
+// else{
+// 	console.log("Halo Ksatria Ellen, kamu dapat menyerang denngan senjatamu!")
+// }
+// if (peran === "Tabib") {
+// 	console.log("Halo Ksatria Ellen, kamu dapat membantu temanmu yang terluka")
+// }
+// else{
+// 	console.log("Hajar terus")
+// }
+
+// var nama ="Sora"
+// var peran="penyihir"
+
+// if (nama !== peran) {
+// 	console.log("Selamat datang di Dunia Proxytia, Sora")
+// }
+// else{
+// 	console.log("Halo Ksatria Sora, kamu dapat menyerang denngan senjatamu!")
+// }
+// if (peran === "penyihir") {
+// 	console.log("Halo Ksatria Sora, kamu dapat menciptkan keajaiban yang membantu kemenangan!")
+// }
+// else{
+// 	console.log("Hajar terus")
+// }
+
+var nama = "Andi";
+var peran= " ";
+
+if (nama === " " ===  peran === " ") {
+console.log('Selamat Bergabung ')
+}
+else {
+console.log('Harus Masukan Nama Disini')
+}
+console.log()
+if (nama === "Andi" ===  peran === " ") {
+	console.log('Selamat Bermain')
+}
+else{
+	console.log('Halo Andi, Pilih Peranmu untuk memulai game')
+}
+console.log()
+if (nama === "Andi" === peran === "Ksatria") {
+	console.log('Harus Bermain')
+}
+else{
+	console.log('Selamat datang di Dunia Proxytia, Andi')
+}
+console.log()
+if (nama !== "Andi" === peran !== "Ksatria") {
+	console.log('Halo Ksatria Andi, Kamu dapat menyerang dengan senjatamu!')
+}
+else{
+	console.log('Silakan logout game')
+}
+console.log()
+if (nama !== "Zero" === peran !== "Tabib") {
+	console.log('Selamat datang di Dunia Proxytia, Zero')
+}
+else{
+	console.log('Selamat Datang')
+}
+console.log()
+if (nama !== "Zero" === peran !== "Tabib" ) {
+	console.log('Halo Tabib Zero, kamu akan membantu temanmu yang terluka')
+}
+else{
+	console.log('Tidak mampu membantu temanmu')
+}
+console.log()
+if (nama !== "Merlin" === peran !== "penyihir") {
+	console.log('Selamat datang di Dunia Proxytia, Merlin')
+}
+else{
+	console.log('Selamat Bermain')
+}
+console.log()
+if (nama !== "Merlin" === peran !=="penyihir") {
+	console.log('Halo penyihir Merlin, Ciptakan keajaiban yang membantu kemenanganmu!')
+}
+else{
+	console.log('Merlin adalah penyihir')
+}
